d340b308d90f0191f0da41527a568fb17ab99dff
kkatterh <kentkatter@gmail.com>
2017-05-22 13:26:39 -0500

    Add files via upload
    Fixed flicker issue by adding in buttonpress variable

--- LED_button_starter-v1.0.0.ino
+++ LED_button_starter-v1.0.0.ino
@@ -11,6 +11,9 @@ int button1state = 1;         // variable for reading the pushbutton status
 int button2state = 1;
 int button3state = 1;
 
+// variable for a button down
+int buttonpress = 0;
+
 void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
@@ -28,24 +31,24 @@ void loop() {
   button2state = digitalRead(button2);
   button3state = digitalRead(button3);
 
+  //button press variable reset to zero
+  buttonpress = 0;
+
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
   if (button1state == HIGH) {
     // turn LED on:
     analogWrite(ledPin, 250);
     Serial.println("HIGH");
-  }   
-  else {
-    // turn LED off:
-    analogWrite(ledPin, 0);
-    Serial.println("LOW");
-  }
+    buttonpress ++;
+  } 
   if (button2state == HIGH) {
     //turn LED on at 175
     analogWrite(ledPin, 175);
     Serial.println("175 YO");
+    buttonpress ++;
   }
-  else {
+  if (buttonpress == 0) {
     // turn LED off:
     analogWrite(ledPin, 0);
     Serial.println("LOW");
